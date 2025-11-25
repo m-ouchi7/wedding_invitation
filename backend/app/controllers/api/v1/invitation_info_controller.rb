e0e8540e16427326135a4fdca0657c0d957b3fc2
ouchi <misaki.m.kirin@gmail.com>
2025-11-25 22:40:59 +0900

    refactor: simplify render json in InvitaionInfoController

--- backend/app/controllers/api/v1/invitation_info_controller.rb
+++ backend/app/controllers/api/v1/invitation_info_controller.rb
@@ -9,16 +9,18 @@ module Api
           return
         end
           
-        render json: {
-          venue_name: invitation_info.venue_name,
-          postal_code: invitation_info.postal_code,
-          address: invitation_info.address,
-          open_time: invitation_info.open_time,
-          start_time: invitation_info.start_time,
-          bride_name: invitation_info.bride_name,
-          groom_name: invitation_info.groom_name,
-          message: invitation_info.message
-        }, status: :ok
+        render json: invitation_info.as_json(
+          only: [
+            :venue_name,
+            :postal_code,
+            :address,
+            :open_time,
+            :start_time,
+            :bride_name,
+            :groom_name,
+            :message
+          ]
+        ), status: :ok
       end
     end
   end
