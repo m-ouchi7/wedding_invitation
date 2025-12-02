e374bafc6e26757afab8b017d0b4e39e14b762a7
ouchi <misaki.m.kirin@gmail.com>
2025-12-02 23:18:09 +0900

    create submit form  in progress (position of submit button and confirm sending)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home'
+import Submit from './pages/Submit'
+import { Box } from "@mui/material"
 
 export default function App() {
   return (
-    <Home />
+    // <Home />
+    <Box
+      display="flex"
+      alignItems="center"
+      sx={{ maxWidth: 393, mx: "auto", p: "0 20px" }}
+    >
+      <Submit />
+    </Box>
   )
 }
